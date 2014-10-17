9f431510596855f26186bc9b0b236be07c9b5f36
jhtobav <jhtobav@gmail.com>
2014-10-17 00:15:02 -0500

    actualizacion
    Juntando cambios

--- src/MyListTest.java
+++ src/MyListTest.java
@@ -8,153 +8,239 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+public class MyListTest{
 
-public class MyListTest {
-	
 	MyList<String> myList1;
+
 	MyList<String> myList2;
-	
+
 	@BeforeClass
 	public static void setUpClass() throws Exception {
-		
-	}
-	
-	@Before
-	public void setUp(){
-		myList1 = new MyList<String>();
-		myList2 = new MyList<String>();
-		
-		myList1.add("a");
-		myList1.add("b");
-		myList1.add("c");
-		
-	}
-	
-	//Test Constructor
-	@Test
-	public void testConstructor(){
-		myList1 = new MyList<String>();
-		assertEquals(0, myList1.size());
-	}
-	
-	//Test Tamano
-	@Test
-	public void testSize() {
-		assertEquals(3, myList1.size());
+
 	}
 
-	//Test Tamano 0 cuando no hay objetos
+	@Before
+	public void setUp() {
+
+		myList1 = new MyList<String>();
+
+		myList2 = new MyList<String>();
+
+		myList1.add("a");
+
+		myList1.add("b");
+
+		myList1.add("c");
+
+	}
+
+	// Test Constructor
+
+	@Test
+	public void testConstructor() {
+
+		myList1 = new MyList<String>();
+
+		assertEquals(0, myList1.size());
+
+	}
+
+	// Test Tamano
+
+	@Test
+	public void testSize() {
+
+		assertEquals(3, myList1.size());
+
+	}
+
+	// Test Tamano 0 cuando no hay objetos
+
 	@Test
 	public void testSizeEmpty() {
+
 		assertEquals(0, myList2.size());
+
 	}
-	
-	//Test Add objeto agregado a ultima posicion
+
+	// Test Add objeto agregado a ultima posicion
 	@Test
 	public void testAdd() {
 		myList1.add("d");
-		assertEquals("d", myList1.get(myList1.size()-1));
+		assertEquals("d", myList1.get(myList1.size() - 1));
 	}
-	
-	//Test Add tamano de lista actualizado
+
+	// Test Add agregar null sin problemas
+	@Test
+	public void testAddNull() {
+		myList1.add(null);
+		assertEquals(null, myList1.get(myList1.size() - 1));
+	}
+
+	// Test Add tamano de lista actualizado
 	@Test
 	public void testAddSize() {
 		int tamanoAntes = myList1.size();
 		myList1.add("d");
-		assertEquals(tamanoAntes+1,myList1.size());
+		assertEquals(tamanoAntes + 1, myList1.size());
 	}
-	
+
+	// Test Add(x,E) objeto agregado en indice especifico
 	@Test
-	public void testAddNull(){
-		myList1.add(null);
+	public void testAddIdx() {
+		myList1.add(1, "d");
+		assertEquals("d", myList1.get(1));
 	}
-	//faltan test de add(x,E)
-	
-	//Test de contains
-	
-	//Revisa si puede encontrar en la lista un valor conocido
+
+	// Test Add tamano de lista actualizado
 	@Test
-	public void testContains(){
+	public void testAddIdxSize() {
+		int tamanoAntes = myList1.size();
+		myList1.add(1, "d");
+		assertEquals(tamanoAntes + 1, myList1.size());
+	}
+
+	// Test Remove
+	@Test
+	public void testRemoveSize() {
+		int tamanoAntes = myList1.size();
+		myList1.remove("d");
+		assertEquals(tamanoAntes + 1, myList1.size());
+	}
+
+	// faltan test de add(x,E)
+
+	// Test de contains
+
+	// Revisa si puede encontrar en la lista un valor conocido
+
+	@Test
+	public void testContains() {
+
 		assertTrue(myList1.contains("a"));
+
 	}
-	
-	//Revisa si devuelve falso para un valor que no se encuentra en la lista
+
+	// Revisa si devuelve falso para un valor que no se encuentra en la lista
+
 	@Test
 	public void testContainsFalse() {
+
 		assertFalse(myList1.contains("k"));
+
 	}
-	
-	//Asegura que un NullPointerException es enviado cuando se envia un objeto nulo
-	@Test (expected=NullPointerException.class)
-	public void testContainsNull(){
+
+	// Asegura que un NullPointerException es enviado cuando se envia un objeto
+	// nulo
+
+	@Test(expected = NullPointerException.class)
+	public void testContainsNull() {
+
 		myList1.contains(null);
+
 	}
-	
-	//Asegura que no interpreta a una cadena vacia como un objeto nulo
+
+	// Asegura que no interpreta a una cadena vacia como un objeto nulo
+
 	@Test
 	public void testContainsEmpty() {
+
 		assertFalse(myList1.contains(""));
+
 	}
-	
-	//Asegura que no puede encontrar ningun tipo de objeto que no se encuentre dentro de la lista
+
+	// Asegura que no puede encontrar ningun tipo de objeto que no se encuentre
+	// dentro de la lista
+
 	@Test
 	public void testContainsObject() {
-		assertFalse(myList1.contains(new Object().toString()));
-	}
-	
-	//faltan test de get
-	
-	//Prueba que la funciÃ³n get este devolviendo el valor correcto para un elemento ya conocido de la lista.
-	@Test
-	public void testGet(){
-		assertEquals(myList1.get(0), "a");
-	}
-	
-	//Prueba que el mÃ©todo get retorne una excepcion en caso que la lista este vacia
-	@Test (expected=IndexOutOfBoundsException.class)
-	public void testGetEmpty(){
-		myList2.get(0);
-	}
-	
-	//Prueba que el mÃ©todo get no retorna el Ãºltimo elemento de la lista cuando se solicita 
-	//un valor mÃ¡s alto que el tamaÃ±o de la misma sino en su lugar devuelve una excepcion.
-	@Test (expected=IndexOutOfBoundsException.class)
-	public void testGetTooBig(){
-		myList1.get(6);
-	}
-	
-	//faltan test de intercalate (E)
-	
-	//Test de intercalate (MyList<E>)
-	//Asumimos que para que dos listas sean intercalables deben tener el mismo tamaÃ±o
-	@Test
-	public void testIntercalate(){
-		myList2.add("1");
-		myList2.add("2");
-		myList2.add("3");
-		
-		assertTrue(myList1.intercalate(myList2));
-	}
-	
-	//Esperamos un false ya que las dos listas tienen tamaÃ±os diferentes
-	@Test
-	public void testIntercalableFalse(){
-		assertFalse(myList1.intercalate(myList2));
-	}
-	
-	//Prueba de intentar realizar un intercalamiento con la lista 2 la cual debe ser imposible debido
-	//a que es un objeto nulo.
-	@Test (expected=NullPointerException.class)
-	public void testIntercalableNull(){
-		myList2 = null;
-		myList1.intercalate(myList2);
-	}
-	
-	//faltan test de join
-	
-	//faltan test de remove(E)
-	
-	//faltan test de sublist(MyList<E>)
 
-	
+		assertFalse(myList1.contains(new Object().toString()));
+
+	}
+
+	// faltan test de get
+
+	// Prueba que la función get este devolviendo el valor correcto para un
+	// elemento ya conocido de la lista.
+
+	@Test
+	public void testGet() {
+
+		assertEquals(myList1.get(0), "a");
+
+	}
+
+	// Prueba que el método get retorne una excepcion en caso que la lista este
+	// vacia
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetEmpty() {
+
+		myList2.get(0);
+
+	}
+
+	// Prueba que el método get no retorna el último elemento de la lista cuando
+	// se solicita
+
+	// un valor más alto que el tamaño de la misma sino en su lugar devuelve una
+	// excepcion.
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetTooBig() {
+
+		myList1.get(6);
+
+	}
+
+	// faltan test de intercalate (E)
+
+	// Test de intercalate (MyList<E>)
+
+	// Asumimos que para que dos listas sean intercalables deben tener el mismo
+	// tamaño
+
+	@Test
+	public void testIntercalate() {
+
+		myList2.add("1");
+
+		myList2.add("2");
+
+		myList2.add("3");
+
+		assertTrue(myList1.intercalate(myList2));
+
+	}
+
+	// Esperamos un false ya que las dos listas tienen tamaños diferentes
+
+	@Test
+	public void testIntercalableFalse() {
+
+		assertFalse(myList1.intercalate(myList2));
+
+	}
+
+	// Prueba de intentar realizar un intercalamiento con la lista 2 la cual
+	// debe ser imposible debido
+
+	// a que es un objeto nulo.
+
+	@Test(expected = NullPointerException.class)
+	public void testIntercalableNull() {
+
+		myList2 = null;
+
+		myList1.intercalate(myList2);
+
+	}
+
+	// faltan test de join
+
+	// faltan test de remove(E)
+
+	// faltan test de sublist(MyList<E>)
+
 }
